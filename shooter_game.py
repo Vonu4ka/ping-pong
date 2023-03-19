6d9177656d6879924a292d3ca0f1382ae654431b
Vonu4ka <127664418+Vonu4ka@users.noreply.github.com>
2023-03-19 15:03:11 +0300

    v0.2
    Добавил отображение мяча

--- shooter_game.py
+++ shooter_game.py
@@ -7,12 +7,43 @@ win_height = 500
 window = display.set_mode((win_width, win_height))
 window.fill(background)
 
+class GameSprite(sprite.Sprite):
+    def __init__(self, player_image, player_x, player_y, player_speed, width, height):
+        self.image = transform.scale(image.load(player_image), (width, height))
+        self.speed = player_speed
+        self.rect = self.image.get_rect()
+        self.rect.x = player_x
+        self.rect.y = player_y
+    def reset(self):
+        window.blit(self.image, (self.rect.x, self.rect.y))
+
+ball = GameSprite('ball.jpg', 200, 200, 4, 50, 50)
+speed_x = 3
+speed_y = 3 
+finish = False
+
 game  = True
 while game:
     for e in event.get():
         if e.type == QUIT:
             game = False
+    if not finish:
+        window.fill(background)
+        #ball.rect.x += speed_x
+        #ball.rect.y += speed_y
+        ball.reset()
     display.update()
+
+
+
+
+
+
+
+
+
+
+
 
 
 
